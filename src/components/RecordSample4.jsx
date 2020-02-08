a55eeb011937735c47f7b0c1b0fb4866119583b7
Jacon Condes <jaconjcondes@gmail.com>
2020-02-08 00:04:47 -0800

    working video record timer

--- src/components/RecordSample4.jsx
+++ src/components/RecordSample4.jsx
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import {ReactMediaRecorder} from 'react-media-recorder';
 
 class RecordSample4 extends Component {
+  timer = (startRecording, stopRecording) => {
+    console.log('Starting');
+    startRecording();
+    setTimeout(function() {
+      console.log('Stopping');
+      stopRecording();
+    }, 5000);
+  };
+
   render() {
     return (
       <div>
@@ -10,9 +19,14 @@ class RecordSample4 extends Component {
           render={({status, startRecording, stopRecording, mediaBlobUrl}) => (
             <div>
               <p>{status}</p>
-              <button onClick={startRecording}>Start Recording</button>
-              <button onClick={stopRecording}>Stop Recording</button>
-              <video src={mediaBlobUrl} controls autoplay loop />
+              <button
+                onClick={() => {
+                  this.timer(startRecording, stopRecording);
+                }}>
+                Start Recording
+              </button>
+              {/* <button onClick={stopRecording}>Stop Recording</button> */}
+              <video src={mediaBlobUrl} controls />
             </div>
           )}
         />
