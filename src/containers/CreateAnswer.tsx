33b05d964c88b4b54dc7a7c112d462d3308c80f8
Jacon Condes <jaconjcondes@gmail.com>
2020-03-16 04:04:48 -0700

    working recorder

--- src/containers/CreateAnswer.tsx
+++ src/containers/CreateAnswer.tsx
@@ -5,9 +5,13 @@ import React, {Component} from 'react';
 
 // import {AppState} from '../redux/rootAppState';
 // import {AppActions} from '../types/rootType.actions';
-import CreateAnswerView from '../views/CreateAnswerView';
+// import CreateAnswerView from '../views/CreateAnswerView';
 
-import {FirebaseFirestore} from '../firebase';
+import Countdown from 'react-countdown';
+import {ReactMediaRecorder} from 'react-media-recorder';
+import VideoPreview from '../components/VideoPreview';
+
+import {FirebaseFirestore, FirebaseStorage} from '../firebase';
 
 interface CreateAnswerPageProps {
   history?: any;
@@ -15,7 +19,10 @@ interface CreateAnswerPageProps {
 }
 
 interface CreateAnswerPageState {
-  answer: any;
+  campaignID: string;
+  questionID: string;
+  question: string;
+  imgURL: string;
 }
 
 export class CreateAnswer extends Component<
@@ -24,43 +31,125 @@ export class CreateAnswer extends Component<
 > {
   constructor(props: any) {
     super(props);
-    this.state = {answer: ''};
+    this.state = {campaignID: '', questionID: '', question: '', imgURL: ''};
   }
 
-  updateAnswer = (value: any) => {
-    this.setState({
-      answer: value,
+  componentDidMount = async () => {
+    const {campaignID, questionID} = this.props.match.params;
+
+    console.log('campaignID + questionID');
+    console.log(campaignID);
+    console.log(questionID);
+    // Query Question
+    await this.getQuestion(campaignID, questionID);
+
+    console.log('Sample');
+    const imgURL = await FirebaseStorage.child('sample').getDownloadURL();
+    await console.log(imgURL);
+
+    await this.setState({
+      imgURL,
     });
   };
 
-  onUpdateAnswer = (event: any) => {
-    this.updateAnswer(event.target.value);
+  getQuestion = async (campaignID: string, questionID: string) => {
+    await console.log('getting single question from firebase');
+
+    await FirebaseFirestore.collection('UndebateQuestions')
+      .doc(questionID)
+      .get()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          console.log('Document data:', doc.data());
+          this.setState({campaignID, questionID: doc.id, ...doc.data()});
+        }
+      });
   };
 
-  createAnswer = async (answer: string) => {
-    const {campaignID} = this.props.match.params;
+  addToStorage = async (mediaBlobUrl: any) => {
+    console.log('addToStorage');
+    console.log(mediaBlobUrl);
+    let blob = await fetch(mediaBlobUrl).then(r => r.blob());
+    console.log(blob);
 
-    await FirebaseFirestore.collection('UndebateAnswers').add({
-      answer: answer,
-      campaignID: campaignID,
-    });
-  };
-
-  onCreateAnswer = (event: any) => {
-    const {campaignID} = this.props.match.params;
-
-    event.preventDefault();
-    this.createAnswer(this.state.answer);
-    this.props.history.push(`/campaign/${campaignID}`);
+    FirebaseStorage.child('sample')
+      .put(blob)
+      .then(function(snapshot: any) {
+        console.log('Uploaded a blob or file!');
+        console.log(snapshot);
+      });
   };
 
   render() {
     return (
-      <CreateAnswerView
-        answer={this.state.answer}
-        onUpdateAnswer={this.onUpdateAnswer}
-        onCreateAnswer={this.onCreateAnswer}
-      />
+      <div>
+        {this.state.imgURL && (
+          <div>
+            <h1>MP$</h1>
+            <video src={this.state.imgURL} controls></video>
+          </div>
+        )}
+
+        <ReactMediaRecorder
+          video
+          render={({
+            status,
+            previewStream,
+            startRecording,
+            stopRecording,
+            mediaBlobUrl,
+          }: any) => (
+            <div>
+              {/* ask for browser permission to record the video and audio */}
+              {status === 'acquiring_media' && (
+                <div>
+                  <h1>Please Give Permission To Access Webcam And Audio</h1>
+                </div>
+              )}
+
+              {/* idle mode */}
+              {status === 'idle' && (
+                <div>
+                  <VideoPreview stream={previewStream} />
+                  <button onClick={() => startRecording()}>
+                    Start Recording
+                  </button>
+                </div>
+              )}
+
+              {/* record with a 60 second countdown */}
+              {status === 'recording' && (
+                <div>
+                  <Countdown
+                    date={Date.now() + 60000}
+                    onComplete={() => stopRecording()}
+                    renderer={({seconds}) => <h1>{seconds}</h1>}
+                  />
+                  <VideoPreview stream={previewStream} />
+                  <button onClick={() => stopRecording()}>
+                    Stop Recording
+                  </button>
+                </div>
+              )}
+
+              {/* convert video blob to .mp4 file in order to upload to s3 */}
+              {status === 'stopped' && (
+                <div>
+                  <video src={mediaBlobUrl} controls />
+                  <button
+                    onClick={() => {
+                      this.addToStorage(mediaBlobUrl);
+                    }}>
+                    Upload
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+        />
+      </div>
     );
   }
 }
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 import {config} from './config';
 
@@ -8,3 +9,4 @@ export const myFirebase = firebase.initializeApp(config);
 
 export const FirebaseAuth = myFirebase.auth();
 export const FirebaseFirestore = myFirebase.firestore();
+export const FirebaseStorage = myFirebase.storage().ref();
